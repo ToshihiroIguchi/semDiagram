0666bfad5d3c73ac110a3f9c8392c5b0c1d2110b
ToshihiroIguchi <toshihiro.iguchi.mail@gmail.com>
2025-04-22 23:05:56 +0900

    add new functions

--- R/semDiagram.R
+++ R/semDiagram.R
@@ -1,17 +1,4 @@
-# lavaanの結果から適合度指標を抜き出してlistにする関数
-fit_indices <- function(model) {
-  fit <- fitMeasures(model)
-  list(
-    pvalue = fit["pvalue"],
-    SRMR   = fit["srmr"],
-    RMSEA  = fit["rmsea"],
-    AIC    = fit["aic"],
-    GFI    = fit["gfi"],
-    AGFI   = fit["agfi"],
-    NFI    = fit["nfi"],
-    CFI    = fit["cfi"]
-  )
-}
+
 
 # lavaan と DiagrammeR, scales パッケージを利用した SEM 図と適合度指標の表示関数
 semDiagram <- function(fitted_model, digits = 3,
